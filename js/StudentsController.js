b31fe91b161c01f58449c80b11d7dd10fd7ee746
Chris Swan & Filipe Silvestre <student@epicodus.com>
2015-09-21 10:28:04 -0700

    add html and angular functions.

--- js/StudentsController.js
+++ js/StudentsController.js
@@ -1,9 +1,15 @@
-studentRoster.controller('StudentsCtrl', function StudentCtro($scope) {
-  $scope.students = [
-    { name: "Sam Schmidt" },
-    { name: "Jessica Martin" },
-    { name: "Sandy Smith" },
-    { name: "Ryan Samuels" },
-    { name: "Brentwood Davis" }
-  ]
+studentRoster.controller('StudentsCtrl', function StudentCtrl($scope) {
+  //create an array of students
+  $scope.students = [];
+
+  //functions
+  $scope.addStudent = function() {
+    $scope.students.push({ name: $scope.studentName });
+    $scope.studentName = null;
+  };
+
+  $scope.deleteStudent = function(student) {
+    var index = $scope.students.indexOf(student);
+    $scope.students.splice(index, 1);
+  };
 });
