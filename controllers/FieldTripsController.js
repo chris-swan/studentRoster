a3c2ad613a0c9c01d23bc8ea6a8e5c620db2fd31
Chris Swan & Sam Martinez <student@epicodus.com>
2015-09-22 10:47:17 -0700

    add stuff

--- controllers/FieldTripsController.js
+++ controllers/FieldTripsController.js
@@ -1,4 +1,5 @@
-studentRoster.controller('FieldTripsCtrl', function FieldTripCtrl($scope) {
+studentRoster.controller('FieldTripsCtrl', function FieldTripCtrl($scope, StudentsFactory) {
+  $scope.students = StudentsFactory.students;
   $scope.studentsWithPermission = function(student) {
     student.permissionSlip = true;
   };
--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -1,4 +1,4 @@
-studentRoster.controller('StudentsCtrl', function StudentCtrl($scope) {
+studentRoster.controller('StudentsCtrl', function StudentCtrl($scope, StudentsFactory) {
   //create an array of students
   $scope.students = StudentsFactory.students;
   $scope.StudentsFactory = StudentsFactory;
